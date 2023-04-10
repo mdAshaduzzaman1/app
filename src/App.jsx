cbee6505c0767c1518e05747ae9ce87e6368fddb
mdAshaduzzaman1 <ashaduzzamannoor19@gmail.com>
2023-04-10 09:08:36 +0600

    header done

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+import './App.css'
+import Header from './Components/Header/Header';
+
+
+
+const App =() => {
+ 
+  return (
+    <div className="App">
+      <Header></Header>
+    </div>
+  )
+}
+export default Header;
+
+
